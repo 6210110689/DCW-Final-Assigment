bc764bd93340e6caedeb4d50271b8247556360e5
6210110689 <6210110689@psu.ac.th>
2022-04-03 16:34:59 +0700

    templeat

--- server/main.js
+++ server/main.js
@@ -14,6 +14,7 @@ const TOKEN_SECRET ='315bf8822770b897b8ae124799b2e34e82036976703b8be051847186729
 app.use(cors())
 app.use(bodyParser.json())
 dotenv.config();
+app.use('/api',Post)
 
 mongoose.connect(process.env.MONGO_URL.toString(), {
   useNewUrlParser: true,
---
+++ web/src/pages/login/Login.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import React,{ useContext, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import "./login.css";
+import FacebookLogin from "react-facebook-login"
+
+export default function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE" });
+    }
+  };
+
+const responseFacebook = async (response) => {
+  if(response.accessToken){
+    console.log('log in with access_Token=' + response.accessToken)
+    let result = await axios.post('http://localhost:8080/api/login', {
+      token: response.accessToken
+    })
+    console.log(result.data)
+    dispatch({ type: "LOGIN_SUCCESS", payload: result.data });
+  }
+}
+
+  return (
+    <div className="login">
+        <FacebookLogin
+        appId="649218682992262"
+        autoLoad={true}
+        fields="name,email,picture"
+        callback={responseFacebook}/>
+        
+      
+    </div>
+  );
+}
